1717e249ef61fefc036a78728fec1faac7ddca77
jfrausto7 <53204698+jfrausto7@users.noreply.github.com>
2023-05-11 18:58:40 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <ThemeProvider theme={themeOptions}>
     <PurrfectProvider>
-      <HashRouter base="/">
+      <HashRouter basename="/">
         <React.StrictMode>
           <App />
         </React.StrictMode>
