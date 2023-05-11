0898dee5e38a0a806b4c0a1370ef092d2d16ce6e
jfrausto7 <53204698+jfrausto7@users.noreply.github.com>
2023-05-11 18:55:07 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import "./App.css"
-import { Routes, Route } from "react-router-dom"
-import { Navbar } from "./components/Navbar"
-import Footer from "./components/Footer"
-import Home from "./pages/Home.js"
-import Adopt from "./pages/Adopt"
-import Volunteer from "./pages/Volunteer"
-import Donate from "./pages/Donate"
-import About from "./pages/About"
-import Login from "./pages/Login"
-import Signup from "./pages/Signup"
-import PetBio from "./pages/PetBio"
-import Favorites from "./pages/Favorites"
-import Profile from "./pages/Profile"
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import { Navbar } from "./components/Navbar";
+import Footer from "./components/Footer";
+import Home from "./pages/Home.js";
+import Adopt from "./pages/Adopt";
+import Volunteer from "./pages/Volunteer";
+import Donate from "./pages/Donate";
+import About from "./pages/About";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+import PetBio from "./pages/PetBio";
+import Favorites from "./pages/Favorites";
+import Profile from "./pages/Profile";
 function App() {
   return (
     <div className="App">
       <Navbar />
       <Routes>
-        <Route path="/" element={<Home />} />
         <Route path="adopt" element={<Adopt />} />
         <Route path="volunteer" element={<Volunteer />} />
         <Route path="donate" element={<Donate />} />
@@ -28,10 +27,11 @@ function App() {
         <Route path="favorites" element={<Favorites />} />
         <Route path="Profile" element={<Profile />} />
         <Route path="/Home" element={<Home />} />
+        <Route path="/" element={<Home />} />
       </Routes>
       <Footer />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
